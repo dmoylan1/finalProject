464941be57783523a148eef14bfd024095bb145b
Danielle Moylan <dmmoylan@sau29.org>
2022-09-27 20:40:06 -0400

    form added to review page

--- src/pages/Reviews.js
+++ src/pages/Reviews.js
@@ -1,9 +1,30 @@
 import React from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 
 function Reviews() {
     return (
-        <div>Customer Reviews</div>
-    )
+        <>        
+        <br />
+        <h4>BettaDirect Customer Reviews</h4>
+        <br />
+        <Form>
+      <Form.Group className="mb-3" controlId="reviewInput">
+        <Form.Label>Enter your review here:</Form.Label>
+        <Form.Control type="text" placeholder="Enter review" />
+        <Form.Text className="text-muted">
+          Please share your experiences with our company here!
+        </Form.Text>
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+
+    </>
+    );
 }
 
 export default Reviews;
